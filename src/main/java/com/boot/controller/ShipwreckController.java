d050ba75a845b63d0ca1ed8b581a798324c96c95
SahanaSanjeeva <spsahana@gmail.com>
2018-07-16 10:11:16 -0500

    Second changes

--- src/main/java/com/boot/controller/ShipwreckController.java
+++ src/main/java/com/boot/controller/ShipwreckController.java
@@ -2,6 +2,8 @@ package com.boot.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +14,28 @@ import com.boot.model.Shipwreck;
 @RequestMapping("/api/v1/")
 public class ShipwreckController {
 
-	@RequestMapping(value = "Shipwrecks", method = RequestMethod.GET)
+	@RequestMapping(value = "shipwrecks", method = RequestMethod.GET)
 	public List<Shipwreck> list() {
 		return ShipwreckStub.list();
+	}
+	
+	@RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.POST)
+	public Shipwreck create(@RequestBody Shipwreck shipwreck) {
+		return ShipwreckStub.create(shipwreck);
+	}
+	
+	@RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.GET)
+	public Shipwreck get(@PathVariable long id) {
+		return ShipwreckStub.get(id);
+	}
+	
+	@RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.PUT)
+	public Shipwreck update(@PathVariable long id, @RequestBody Shipwreck shipwreck) {
+		return ShipwreckStub.update(id, shipwreck);
+	}
+	
+	@RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.DELETE)
+	public Shipwreck delete(@PathVariable long id) {
+		return ShipwreckStub.delete(id);
 	}
 }
